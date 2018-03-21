cd82aed5cf91b714bf11abec0919208030705b24
Frede Larouche <frederique.larouche@gmail.com>
2018-03-21 14:51:09 -0400

    Un peu de chair sur l'os

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './css/main.css';
+import './scss/main.scss';
+import {hello, sup, inc} from './js/module';
+
+
+hello();
+//sup();
+
+
+let epiceriesChoisies = ["bacon cheeseburger", "chicken sandwich", "hamburger", "bacon salad"]
+            .filter((x) => /bacon/i.test(x) );
+console.log("Épiceries : '" + epiceriesChoisies);
+
+
+console.log("Incrémente 6 de 1 : " + inc(6) );
